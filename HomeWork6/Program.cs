16907819cac03ef1695ddfcf94ac1f65e48ebb44
Evgeniy Troitskiy <T89266455873@yandex.ru>
2022-07-14 23:35:43 +0400

    Финиш

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,55 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+
+/*void Numbers(int m)
+{
+    int counter = 0;
+    for(int i = 0; i<m;i++)
+    
+    {
+        
+        Console.WriteLine($"Введи число {i+1}: ");
+        int numbers = Convert.ToInt32(Console.ReadLine());
+        if(numbers>0)counter++;
+        
+    }
+    Console.WriteLine($"Чисел больше нуля :  {counter}");
+}
+
+Console.Write("Введи число М(количество чисел): \n");
+int m = Convert.ToInt32(Console.ReadLine());
+Numbers(m);*/
+
+
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+
+Console.Write("К1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("К2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("B1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("B2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+int x1 = b2 - b1;
+int x2 = k1 - k2;
+int x = x1 / x2;
+int y1 = k1 * x + b1;
+int y2 = k2 * x + b2;
+
+Convert.ToInt32(Console.ReadLine());
+Console.Write(x, y1, y2); // Здесь поплыл, не знаю как исправить
+
+
+
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿/*int[] ReverseArray(int [] array)
+{
+    for(int i = 0, j = array.Length - 1; i < j; i++, j--)
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}*/
+
+//Задача которая преобразовывает десятичное число в двоичное
+
+/*string ConvertNumber(int num)
+{
+    string ConNum = string.Empty;
+    while(num > 0)
+    {
+     ConNum = num % 2 + ConNum;
+     num = num/2;
+    }
+    return ConNum;
+}
+Console.WriteLine(ConvertNumber(36));*/
+
+// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи : 0 и 1.
+
+/*int[] Fibonachi(int N)
+{
+    int[] array = new int[N];
+    array[0] = 0;
+    array[1] = 1;
+
+    for (int i = 2; i < N; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+
+}
+void ShowArray(int[] array)
+{
+for(int i=0;i<array.Length;i++)
+   Console.Write(array[i] + " ");
+Console.WriteLine();
+}
+
+
+ShowArray(Fibonachi(5));*/
+//Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины. (каждая сторона меньше двух других)
+// A< B+C
+/*bool IsTriangle(int n1, int n2, int n3)
+{
+    return n1 < n2 + n3 && n2 < n1 + n3 && n3 < n1 + n2; // 1 способ решения, самый лаконичный
+
+    //if(n1<n2+n3 && n2<n1+n3 && n3<n1+n2) return true;   2 способ
+    //else return false;
+}
+
+Console.WriteLine("Impet number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Impet number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Impet number: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(IsTriangle(n1, n2, n3));*/
